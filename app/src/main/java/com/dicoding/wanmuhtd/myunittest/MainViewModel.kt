f17937998ee1a267b7bfe3479be73f8ad0d6774e
wanmuhtd <ryskrnzo@gmail.com>
2024-10-20 22:30:14 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/wanmuhtd/myunittest/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.dicoding.wanmuhtd.myunittest
+
+class MainViewModel(private val cuboidModel: CuboidModel) {
+    fun getCircumference() = cuboidModel.getCircumference()
+    fun getSurfaceArea() = cuboidModel.getSurfaceArea()
+    fun getVolume() = cuboidModel.getVolume()
+
+    fun save(l: Double, w: Double, h: Double) {
+        cuboidModel.save(l, w, h)
+    }
+}
